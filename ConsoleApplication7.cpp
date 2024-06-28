a86972b4359111f6337f548795b9f3319744e01a
student <student@GKK-367ST-225>
2024-06-28 14:45:28 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <uWS/uWS.h>
+#include <opcode.def>
+
+
+struct PerSocketData {
+	// Ваша структура данных для каждого сокета
+};
+
+int main() {
+	uWS::App().ws<PerSocketData>("/*", {
+	.compression = uWS::SHARED_COMPRESSOR,
+	.maxPayloadLength = 16 * 1024 * 1024,
+	.idleTimeout = 16,
+	.maxBackpressure = 1 * 1024 * 1024,
+	.closeOnBackpressureLimit = false,
+	.resetIdleTimeoutOnSend = false,
+	.sendPingsAutomatically = true,
+	.upgrade = nullptr,
+	.open = [](auto* ws) {
+			// Open event
+			},
+			.message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
+			ws->send(message, opCode);
+			},
+			.drain = [](auto* ws) {
+				// Check ws->getBufferedAmount() here
+				},
+				.close = [](auto* ws, int code, std::string_view message) {
+					// Close event
+					}
+		}).listen(8080, [](auto* token) {
+						if (token) {
+							std::cout « "Listening on port " « 8080 « std::endl;
+						}
+			}).run();
+}
